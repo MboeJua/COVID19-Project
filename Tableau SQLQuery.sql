d4731c57e1a15bf70f26e1897dbfcab1d5d377a8
Paulinus Jua <87013817+MboeJua@users.noreply.github.com>
2023-06-20 15:40:36 +0100

    Update Tableau SQLQuery.sql

--- Tableau SQLQuery.sql
+++ Tableau SQLQuery.sql
@@ -8,9 +8,7 @@ Queries used for Tableau Project
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From CovidDeaths
---Where location like '%states%'
 where continent is not null 
---Group By date
 order by 1,2
 
 -- 1a. 
